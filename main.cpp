676d97f60513f8eac1e60abbd3d06d715d8a9ec2
Vale088 <valerio.sinni@marconibari.edu.it>
2025-10-01 17:13:01 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,45 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int N,i,j;
+    cout<<"Quante cifre vuoi inserire?"<<endl;
+    cin>>N;
+    while(N<=0) {
+        cout<<"inserire un valore maggiore di 0"<<endl;
+        cin>>N;
+        cout<<i+1<<"Numero:"<<R[i]<<endl;
 
+    }   
+    int R[N];
+    i=0;
+    while(i<N) {
+        R[i]=random(n*10);
+        i=i+1;
+    } 
+    bool scambio;
+    scambio=true;
+    i=0;
+    while(i<=N-1 and scambio ) {
+        scambio=false;
+        j=0;
+        while(j<=(n-2)-i) {
+            if(R[j]>R[j+1]) {
+                t=R[i]
+                R[j]=R[j+1];
+                R[j+1]=t;
+
+
+            } 
+            j=j+1;
+        }  
+        i=i+1; 
+
+
+    } 
+    i=0;
+    while(i<N) {
+        cout<<"R[i]"<<endl;
+        i=i+1;
+    } 
+     }
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
